7eb5cd97bd9c9a385fab12a43399253fb6e4e50e
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-31 16:18:58 -0700

    changes to game constants

---
+++ src/components/LilTetris.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+import { KeyCodes } from '../constants/Game';
+import Action from '../actions/action';
+
+export default class LilTetris extends Component {
+    
+}
