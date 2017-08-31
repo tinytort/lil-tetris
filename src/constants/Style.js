04bc62c615cfc9ff5358987236987f729cf3e0da
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-31 12:18:03 -0700

    begin work on actions and constants (Game and Style)

---
+++ src/constants/Style.js
@@ -0,0 +1,16 @@
+export const Colors = {
+    Field: {
+        backgroundColor: '#1b2b34',
+        border: '#1b2b34'
+    },
+
+    Blocks: [
+        '#ec5f67',
+        '#99c794',
+        '#c594c5',
+        '#6699cc',
+        '#5fb3b3',
+        '#fac863',
+        '#f99157'
+    ]
+};
