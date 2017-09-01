bc62f60292ed1fd7d77146716e047578b42db967
Haley Tortorella <hdtort@gmail.com>
2017-09-01 09:50:48 -0700

    GameField file initialized

--- src/components/GameField.js
+++ src/components/GameField.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import Banner from './Banner';
+import CurrentBlock from '../containers/CurrentBlock';
+import ActiveBlock from '../containers/ActiveBlock';
 import gameConstants from '../constants/gameConstants.js';
+
 
 const { fieldHeight, fieldWidth } = gameConstants;
 
@@ -14,10 +17,22 @@ let GameField = ({ isPlaying, isPaused, isGameOver }) => {
                         <CurrentBlock />
                         <ActiveBlocks />
                     </div>
+                    { isPaused ? <Banner label="PAUSED" /> : null }
                 </div>
+                { isGameOver ? <Banner label="GAME OVER" /> : null }
             </div>
-        )
+        );
     }
     return null;
 };
+
+const mapStateToProps = ({ gameStatus }) => ({
+    isPlaying: gameStatus !== 'IDLE', 
+    isPaused: gameStatus === 'PAUSED',
+    isGameOver: gameStatus === 'GAME_OVER'
+});
+
+GameField = connect(mapStateToProps)(GameField);
+
+export default GameField;
 
