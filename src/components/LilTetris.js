da9620cc85c7b5ded6ef7b0484ae794a17835346
Haley Tortorella <hdtort@gmail.com>
2017-09-01 10:13:59 -0700

    Fixed LilTetris and imported other files

--- src/components/LilTetris.js
+++ src/components/LilTetris.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import GameField from './GameField.js';
 import gameConstants from '../constants/gameConstants';
+import MenuContainer from '../containers/MenuContainer';
+import CurrentGameInfo from '../containers/CurrentGameInfo';
+
 
 const { fieldWidth, fieldHeight } = gameConstants;
 
 const LilTetris = () => (
     <div>
+        <div>
+            <MenuContainer /> 
+        </div>
         <GameField width={fieldWidth} height={fieldHeight} />
+        <CurrentGameInfo />
     </div>
 );
 
