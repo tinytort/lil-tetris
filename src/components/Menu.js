0cd3ed919163caed866a4510cc13fb16664564ba
Haley Tortorella <hdtort@gmail.com>
2017-09-01 10:11:41 -0700

    Menu is set up for start of game

---
+++ src/components/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { loadMenu } from '../actions/action';
+
+class Menu extends React.Component {
+    componentDidMount(){
+        this.props.dispatch(loadMenu());
+    }
+    render() {
+        return (
+            <div>
+                <h1>Lil Tetris</h1>
+                { !this.props.isPlaying ? <h2>Press space to start</h2> : null }
+            </div>
+        );
+    }
+}
+
+Menu.propTypes = {
+    isPlaying: PropTypes.string
+};
+
+export default Menu;
