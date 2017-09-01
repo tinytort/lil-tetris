621e5e0c33d411614c255339209971eecae30564
Haley Tortorella <hdtort@gmail.com>
2017-09-01 09:43:33 -0700

    Banner file set up

---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { startGame } from '../actions/action';
+import { connect } from 'react-redux';
+
+let Banner = ({ label, dispatch }) => {
+    return (
+        <div>
+            <h1>{label}</h1>
+            { label === 'GAME OVER' } ? 
+                <button onClick={()=>dispatch(startGame())}>New Game </button> : null}
+        </div>
+    );
+};
+
+Banner = connect()(Banner);
+export default Banner;
